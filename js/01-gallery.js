cca9277b39463ccdd0842ec59a164cb0a7fee79d
userzahar <110730909+userzahar@users.noreply.github.com>
2022-12-31 17:14:18 +0200

    ніби 1 завдання виконано

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,37 +1,43 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
-
 const galleryContainer = document.querySelector(".gallery");
 
-galleryContainer.addEventListener("click", onGalleryClick)
+galleryContainer.addEventListener("click", onGalleryClick);
 
 function fillGalleryBox(itemsArr) {
-    const image = itemsArr.map(img => {
-        return `
+  const image = itemsArr.map(img => {
+    return `
     <div class="gallery__item">
     <a class="gallery__link" href="${img.original}">
-      <img 
-        class="gallery__image"
-        src="${img.preview}"
-        data-source="${img.original}"
-        alt="${img.description}"
-      />
+    <img 
+    class="gallery__image"
+    src="${img.preview}"
+    data-source="${img.original}"
+    alt="${img.description}"
+    />
     </a>
-  </div>`
-}).join('');
-// console.log("🚀 ", image);
-galleryContainer.innerHTML = image;
+    </div>`
+  }).join('');
+  galleryContainer.innerHTML = image;
 }
 
 function onGalleryClick(events) {
-    events.preventDefault();
-    const { target } = events;
-    if (target.nodeName === "IMG") {
-        console.log(target.dataset.source)
+  events.preventDefault();
+  const { target } = events;
+  if (target.nodeName !== "IMG") {
+    return;
+  } 
+  const instance = basicLightbox.create(`
+  <img src="${target.dataset.source}" width="800" height="600">`);
+  instance.show();
+  
+  galleryContainer.addEventListener("keydown", onEscapeClick);
+  function onEscapeClick(events) {
+    if (events.code === "Escape") {
+        instance.close()
+      }      
     }
-}
-
-
-fillGalleryBox(galleryItems);
-
-// console.log(galleryItems);
+  }
+  
+  
+  fillGalleryBox(galleryItems);
