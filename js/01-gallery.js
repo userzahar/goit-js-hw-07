2be6579f90b2b55a395c75e5ae22030dd997400d
userzahar <110730909+userzahar@users.noreply.github.com>
2022-12-31 13:11:17 +0200

    створив галерею

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,37 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector(".gallery");
+
+galleryContainer.addEventListener("click", onGalleryClick)
+
+function fillGalleryBox(itemsArr) {
+    const image = itemsArr.map(img => {
+        return `
+    <div class="gallery__item">
+    <a class="gallery__link" href="${img.original}">
+      <img 
+        class="gallery__image"
+        src="${img.preview}"
+        data-source="${img.original}"
+        alt="${img.description}"
+      />
+    </a>
+  </div>`
+}).join('');
+// console.log("🚀 ", image);
+galleryContainer.innerHTML = image;
+}
+
+function onGalleryClick(events) {
+    events.preventDefault();
+    const { target } = events;
+    if (target.nodeName === "IMG") {
+        console.log(target.dataset.source)
+    }
+}
+
+
+fillGalleryBox(galleryItems);
+
+// console.log(galleryItems);
